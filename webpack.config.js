b22e56f57bc21bf7223b964fe963811479c3af76
Ryohei Nishiyama <ryouhei222@gmail.com>
2018-09-30 02:28:39 +0900

    add: webpack設定ファイル

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+    entry: './webapp/src/js/app.js',
+    devtool: 'inline-source-map',
+    cache: true,
+    debug: true,
+    output: {
+        path: __dirname,
+        filename: './webapp/build/build.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
