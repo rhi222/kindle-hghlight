c0be8bffc4efbfe2be7c3bec43c2e1844a2ec699
Ryohei Nishiyama <ryouhei222@gmail.com>
2018-09-30 01:29:27 +0900

    add: jsonを返却するwebサーバをflaskで構築

---
+++ flask/index.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify
+import os
+import json
+
+
+app = Flask(__name__)
+
+
+def get_kindle_data():
+    json_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "../data-generator",
+            "kindle.json"
+    )
+    with open(json_path) as f:
+        data = json.loads(f.read(), 'utf-8')
+    return data
+
+
+@app.route('/')
+def hello():
+    name = "Hello World"
+    return name
+
+
+@app.route('/api/json')
+def api_json():
+    response = jsonify(get_kindle_data())
+    response.status_code = 200
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
