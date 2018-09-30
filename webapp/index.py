d2a677a379bfcd521aad8027f9ad41f9003d841a
Ryohei Nishiyama <ryouhei222@gmail.com>
2018-09-30 09:25:48 +0900

    add: randomに抽出した書籍情報を返却するapi

--- webapp/index.py
+++ webapp/index.py
@@ -2,8 +2,14 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, render_template, send_from_directory
 import os
+import sys
 import json
-
+sys.path.append(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    '../slack-messenger'
+    )
+)
+from post_slack import get_target_book_info  # noqa E402
 
 app = Flask(__name__, static_url_path='')
 app.config["CACHE_TYPE"] = "null"
@@ -36,9 +42,19 @@ def hello():
     return name
 
 
-@app.route('/api/json')
+@app.route('/api/json/all')
 def api_json():
+    print('---- json all')
     response = jsonify(get_kindle_data())
+    response.status_code = 200
+    return response
+
+
+@app.route('/api/json/book')
+def book_json():
+    print('---- json book')
+    book_data = get_target_book_info(get_kindle_data())
+    response = jsonify(book_data)
     response.status_code = 200
     return response
 
